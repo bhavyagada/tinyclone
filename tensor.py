d8f321e17f2f2bce8588ba9c60947701ede03adf
bhavya <bsg@somaiya.edu>
2024-10-01 16:03:00 -0700

    logsoftmax and mul work + a new test

--- tensor.py
+++ tensor.py
@@ -4,29 +4,71 @@ import numpy as np
 # **** start with three base classes ****
 
 class Context:
-  def __init__(self):
+  def __init__(self, arg, *tensors):
+    self.arg = arg
+    self.parents = tensors
     self.saved_tensors = []
   
   def save_for_backward(self, *x):
     self.saved_tensors.extend(x)
 
 class Tensor:
-  def __init__(self, data, _children=()):
+  def __init__(self, data):
+    if type(data) != np.ndarray:
+      print("error constructing tensor with %r" % data)
+      assert(False)
     self.data = data
-    self.grad = np.zeros_like(data)
+    self.grad = None
 
     # internal variables used for autograd graph construction
-    self._prev = set(_children)
+    self._ctx = None
+  
+  def __str__(self):
+    return "Tensor %r with grad %r" % (self.data, self.grad)
+  
+  def backward(self, allow_fill=True):
+    if self._ctx is None:
+      return
+
+    if self.grad is None and allow_fill:
+      # fill in the first grad with one
+      assert self.data.size == 1
+      self.grad = np.ones_like(self.data)
+
+    assert self.grad is not None
+
+    grads = self._ctx.arg.backward(self._ctx, self.grad)
+    if len(self._ctx.parents) == 1:
+      grads = [grads]
+    for t, g in zip(self._ctx.parents, grads):
+      if g.shape != t.data.shape:
+        print("grad shape must match tensor shape in %r, %r != %r" % (self._ctx.arg, g.shape, t.data.shape))
+        assert(False)
+      t.grad = g
+      t.backward(False)
 
 class Function:
   def apply(self, arg, *x):
-    ctx = Context()
-    x = [self]+list(x)
-    ret = Tensor(arg.forward(ctx, *[t.data for t in x]))
+    ctx = Context(arg, self, *x)
+    ret = Tensor(arg.forward(ctx, self.data, *[t.data for t in x]))
+    ret._ctx = ctx
     return ret
 
 def register(name, fxn):
   setattr(Tensor, name, partialmethod(fxn.apply, fxn))
+
+# **** implement a few functions ****
+class Mul(Function):
+  @staticmethod
+  def forward(ctx, x, y):
+    ctx.save_for_backward(x, y)
+    return x*y
+  
+  @staticmethod
+  def backward(ctx, grad_output):
+    x, y = ctx.saved_tensors
+    return y*grad_output, x*grad_output
+register('mul', Mul)
 
 class ReLU(Function):
   @staticmethod
@@ -37,7 +79,7 @@ class ReLU(Function):
   @staticmethod
   def backward(ctx, grad_output):
     input, = ctx.saved_tensors
-    grad_input = grad_output.clone()
+    grad_input = grad_output.copy()
     grad_input[input < 0] = 0
     return grad_input
 register('relu', ReLU)
@@ -52,7 +94,7 @@ class Dot(Function):
   def backward(ctx, grad_output):
     input, weight = ctx.saved_tensors
     grad_input = grad_output.dot(weight.T)
-    grad_weight = grad_output.dot(input)
+    grad_weight = grad_output.T.dot(input).T
     return grad_input, grad_weight
 register('dot', Dot)
 
@@ -60,11 +102,26 @@ class Sum(Function):
   @staticmethod
   def forward(ctx, input):
     ctx.save_for_backward(input)
-    return input.sum()
+    return np.array(input.sum())
   
   @staticmethod
   def backward(ctx, grad_output):
-    input = ctx.saved_tensors
-    return grad_output * input
+    input, = ctx.saved_tensors
+    return grad_output * np.ones_like(input)
 register('sum', Sum)
 
+class LogSoftmax(Function):
+  @staticmethod
+  def forward(ctx, input):
+    def logsumexp(x):
+      c = x.max(axis=1)
+      return c + np.log(np.exp(x-c.reshape((-1, 1))).sum(axis=1))
+    output = input - logsumexp(input).reshape((-1, 1))
+    ctx.save_for_backward(output)
+    return output
+  
+  @staticmethod
+  def backward(ctx, grad_output):
+    output, = ctx.saved_tensors
+    return grad_output - np.exp(output)*grad_output.sum(axis=1).reshape((-1, 1))
+register('logsoftmax', LogSoftmax)
--- test.py
+++ test.py
@@ -1,45 +1,36 @@
+import torch
 import numpy as np
 from tensor import Tensor
 
-x = np.random.randn(1, 3)
-W = np.random.randn(3, 3)
-npout = x.dot(W)
-print("numpy")
-print(x, end="\n")
-print(W, end="\n")
-print(npout, end="\n")
+np.set_printoptions(precision=3)
+x_init = np.random.randn(1,3).astype(np.float32)
+W_init = np.random.randn(3,3).astype(np.float32)
+m_init = np.random.randn(1,3).astype(np.float32)
 
-x = Tensor(x)
-W = Tensor(W)
-tinyout = x.dot(W)
-print("tinygrad")
-print(x.data, end="\n")
-print(W.data, end="\n")
-print(tinyout.data, end="\n")
+def test_tinyclone():
+  x = Tensor(x_init)
+  W = Tensor(W_init)
+  m = Tensor(m_init)
+  out = x.dot(W)
+  outr = out.relu()
+  outl = outr.logsoftmax()
+  outm = outl.mul(m)
+  outx = outm.sum()
+  outx.backward()
+  return outx.data, x.grad, W.grad
 
-assert np.array_equiv(npout, tinyout.data), "dot results do not match"
+def test_pytorch():
+  x = torch.tensor(x_init, requires_grad=True)
+  W = torch.tensor(W_init, requires_grad=True)
+  m = torch.tensor(m_init)
+  out = x.matmul(W)
+  outr = out.relu()
+  outl = torch.nn.functional.log_softmax(outr, dim=1)
+  outm = outl.mul(m)
+  outx = outm.sum()
+  outx.backward()
+  return outx.detach().numpy(), x.grad, W.grad
 
-x = np.random.randn(1, 3)
-x_tensor = Tensor(x)
-print(x, end="\n")
-print(x_tensor.data, end="\n")
-
-np_sum = np.sum(x)
-print("numpy sum")
-print(np_sum, end="\n")
-
-tiny_sum = x_tensor.sum()
-print("tinygrad sum")
-print(tiny_sum.data, end="\n")
-
-assert np.array_equal(np_sum, tiny_sum.data), "Sum results do not match!"
-
-np_relu = np.maximum(x, 0)
-print("numpy ReLU")
-print(np_relu, end="\n")
-
-tiny_relu = x_tensor.relu()
-print("tinygrad ReLU")
-print(tiny_relu.data, end="\n")
-
-assert np.array_equal(np_relu, tiny_relu.data), "ReLU results do not match!"
+for x, y in zip(test_tinyclone(), test_pytorch()):
+  print(x, y)
+  np.testing.assert_allclose(x, y, atol=1e-6)
